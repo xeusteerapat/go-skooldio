09527cf9c07321584277e52333fe3475923e14c5
xeusteerapat <xeus085@gmail.com>
2022-07-21 09:05:12 +0700

    :sparkles: [Add]

--- 08_go_advanced_concept/wait_group.go
+++ 08_go_advanced_concept/wait_group.go
@@ -13,15 +13,15 @@ func main() {
 	wg.Add(3)
 
 	start := time.Now()
-	go doSomething(wg)
-	go doSomething(wg)
-	go doSomething(wg)
+	go doSomething2(wg)
+	go doSomething2(wg)
+	go doSomething2(wg)
 	// time.Sleep(120 * time.Millisecond)
 	wg.Wait()
 	fmt.Println(time.Since(start))
 }
 
-func doSomething(wg *sync.WaitGroup) {
+func doSomething2(wg *sync.WaitGroup) {
 	time.Sleep(100 * time.Millisecond)
 	fmt.Println("Something func")
 	wg.Done()
